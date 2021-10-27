283a676255d5e771d11391f7c01cf0dae47cf1d4
LutsenkoVitaliy <lutsenko996@gmail.com>
2021-10-27 13:24:40 +0300

    1st

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,9 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
 
 console.log(galleryItems);
 const renderGallery = items => {
@@ -24,20 +27,4 @@ const galleryRef = document.querySelector('.gallery')
 const imgGalleryEl = galleryItems.map(renderGallery).join('') 
 galleryRef.insertAdjacentHTML('beforeend', imgGalleryEl)
 
-galleryRef.addEventListener('click', event => {
-    event.preventDefault();
-
-    if (event.target.nodeName !== 'IMG') {
-        return
-    };
-    
-    let srcLink = event.target.getAttribute("data-source");
-    console.log(srcLink);
-
-    window.addEventListener("keydown", function (evt) {
-            if (evt.key === "Escape") {
-                instance.close();
-            };
-        });
-
-});
+new SimpleLightbox('.gallery a');
