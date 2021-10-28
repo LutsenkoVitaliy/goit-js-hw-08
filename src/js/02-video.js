e66ee1e01b40473c4751e2ec53ec6d51b6119b63
LutsenkoVitaliy <lutsenko996@gmail.com>
2021-10-28 19:41:48 +0300

    2st

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,34 @@
 import throttle from 'lodash/throttle'
-var iframe = document.querySelector('iframe');
-var player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const onUpdate = data => {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+};
+player.on('timeupdate', onUpdate);
+
+const time = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(time)
+    .then(seconds => {
+        console.log('hello world');
+    })
+    .catch(() => console.log(oshibka));
+
+
+
+
+
+// function populateStorage() {
+//   localStorage.setItem("videoplayer-current-time", "oficiant gay");
+// }
+
+// populateStorage();
+
+// const playerTime = localStorage.getItem("videoplayer-current-time");
+// console.log(playerTime);
+
+// function remove() {
+//     localStorage.removeItem("videoplayer-current-time")
+// }
+
+// remove()
